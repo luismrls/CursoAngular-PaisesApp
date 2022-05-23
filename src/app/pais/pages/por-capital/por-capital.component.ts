ff416cd0b534ec67a96113baaaefddcd0487c041
Luis Morales <dev.luismorales@gmail.com>
2022-05-23 17:20:59 -0500

    se completo el componente por capital y se creo el servicio para el mismo

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
-  ngOnInit(): void {
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPorCapital(this.termino).subscribe({
+      next: (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      error: (err) => {
+        this.hayError = true;
+        this.paises = [];
+        console.log('Error')
+        console.info(err)
+      }
+    });
+  }
+
+
+  sugerencias( event: string ) {
+    this.hayError = false;
+
   }
 
 }
