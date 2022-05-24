7788e92f9c51ed5051a1b77109b68df8c8e778f8
Luis Morales <dev.luismorales@gmail.com>
2022-05-24 12:00:13 -0500

    se agrego la sugerencias al buscar por pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,7 +5,11 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
+  styles: [`
+  li {
+    cursor: pointer;
+  }
+  `
   ]
 })
 export class PorPaisComponent {
@@ -13,10 +17,14 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -35,8 +43,20 @@ export class PorPaisComponent {
   }
 
 
-  sugerencias( event: string ) {
+  sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino).subscribe({
+      next: (paisesSugeridos) => this.paisesSugeridos = paisesSugeridos.splice(0, 5),
+      error: () => this.paisesSugeridos = []
+    })
+
+  }
+
+  buscarSugerido( termino: string) {
+    this.buscar(termino);
 
   }
 }
